9571e8baa07b2b508a18a64721d6e5ae6f692ccf
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-04-23 23:23:37 +1000

    Shape is becoming clearer

--- src/hypothesis/extra/types.py
+++ src/hypothesis/extra/types.py
@@ -39,6 +39,7 @@ The latter will be particularly annoying.
 from __future__ import division, print_function, absolute_import
 
 import collections
+import contextlib
 import decimal
 import fractions
 import functools
@@ -56,67 +57,65 @@ from hypothesis.errors import InvalidArgument
 
 StratLookup = Dict[type, st.SearchStrategy]
 
+class ResolutionFailed(TypeError, InvalidArgument):
+    """Raised when a type cannot be resolved to a strategy."""
+    pass
+
 
 def resolve(thing: typing.Any, lookup: StratLookup=None) -> st.SearchStrategy:
     # DRAFT ONLY
-    """Resolve a type to an appropriate search strategy.
-
-    ``thing`` may be a type, including the generic types from the typing
-    module, or a type-annotated function.  Not all values which match a
-    type can always be drawn from the strategy; some shortcuts or simpler
-    (but still matching) values may be used.  For annotated functions,
-    we may fall back to defaults etc. if some annotations cannot be
-    resolved.
+    """Resolve a type or annotated function to an appropriate search strategy.
 
     This function basically does dispatch to more specialised resolvers,
-    when the sheer generality is useful.
+    but the generality is very useful when resolving recursively.
+
+    1. If ``thing`` is a type that can be drawn from builtin strategies
+       (plus datetime and numpy extra strategies if available), or is in the
+       ``lookup`` mapping you supply, the corresponding strategy is returned.
+       Note that you can put any key in the lookup mapping, not just types -
+       allowing you to e.g. override the strategy chosen for a function.
+
+    2. If ``thing`` is a generic type from the typing module, the resolution
+       logic is similar.  As these types are typically specialised (e.g.
+       ``List[int]`` -> ``lists(integers())``), no further customisation is
+       possible for these types.
+
+    3. If ``thing`` is an function, each argument will be resolved to a
+       strategy.  The returned strategy draws a value for each argument,
+       calls the function, and returns the result.  All arguments must have
+       either a resolvable type annotation or a default value.
+
+       TODO: or possibly a strategy over zero-argument callables, to force
+           exception handling back to the user?  Could store the arguments
+           on an attribute too; useful for eg round-trip tests...
+
+    Note that while types are used to infer the appropriate strategy,
+    Hypothesis will not check that returned values (etc) match the declared
+    types - you will need to write those tests for yourself.
 
     """
-    # Try direct type lookup first
-    known_types = type_strategy_mapping(lookup)
-    if thing in known_types:
-        # This returns registered strategies for an exact type.
-        # We try subtypes last, after checking special and container types.
-        return known_types[thing]
+    # First, try looking up the type in our mapping of things to strategies
+    if thing in type_strategy_mapping(lookup):
+        return type_strategy_mapping(lookup)[thing]
 
     # Any, Union, Optional, TypeVar, etc - 'special types'
     if thing is typing.Any:
         # Any is a particularly special case
         # Is this really the right way to handle it?  (if not fix TypeVar too)
-
         class CouldBeAnything(object):
             pass
         return st.builds(CouldBeAnything)
 
     if issubclass(thing, Union):
-        params = thing.__union_params__
-        if type(None) in params:
-            # Minimise to None for Optional[T]
-            params = (type(None),) + tuple(
-                t for t in params if t is not type(None))
-        return st.one_of([resolve(t) for t in params])
-
-    if isinstance(thing, TypeVar):
-        # We can treat a constrained TypeVar as a Union of the constraining
-        # types.  An unconstrained typevar should stand in for all types,
-        # so we can test it with a subtype of `object`.
-        if not thing.__constraints__:
-            return resolve(typing.Any)
-        return st.one_of([resolve(t) for t in params])
-
+        return st.one_of([resolve(t) for t in thing.__union_params__])
+    if isinstance(thing, TypeVar) and thing.__constraints__:
+        # A constrained TypeVar is like a Union of the constraining types
+        return st.one_of([resolve(t) for t in thing.__constraints__])
     if isinstance(thing, typing.GenericMeta):
         # Split out into it's own (large) function
         return strategy_from_generic_type(thing)
 
-    # This is the subtype lookup mentioned above.
-    subtype_of = [t for t in known_types
-                  if isinstance(t, type) and issubclass(thing, t)]
-    if subtype_of:
-        no_subtypes = [t for t in subtype_of if sum(
-            issubclass(st, t) for st in subtype_of) == 1]
-        return st.one_of([known_types[t] for t in no_subtypes]).map(thing)
-
-    raise InvalidArgument("Could not find strategy for type %s" % thing)
+    raise ResolutionFailed("Could not find strategy for type %s" % thing)
 
 
 @functools.lru_cache()
@@ -125,9 +124,6 @@ def type_strategy_mapping(lookup: StratLookup=None) -> StratLookup:
 
     Most resolutions will terminate here or in the special handling for
     generics from the typing module.
-
-    TODO: take a supplementary argument of user-defined type: strategy
-          relationships and include them in the lookup somehow.
 
     """
     known_type_strats = {
@@ -147,7 +143,8 @@ def type_strategy_mapping(lookup: StratLookup=None) -> StratLookup:
         frozenset: st.frozensets(st.nothing()),
         dict: st.dictionaries(st.nothing(), st.nothing()),
     }
-    try:
+    # TODO: add the equivalent entry for extra.django - model lookup??
+    with contextlib.suppress(ImportError):
         import datetime as dt
         import  hypothesis.extra.datetime as dt_strats
         known_type_strats.update({
@@ -156,9 +153,7 @@ def type_strategy_mapping(lookup: StratLookup=None) -> StratLookup:
             dt.time: dt_strats.times(),
             # TODO: add timedeltas once pull is merged
         })
-    except ImportError:
-        pass
-    try:
+    with contextlib.suppress(ImportError):
         import numpy as np
         from hypothesis.extra.numpy import \
             arrays, array_shapes, scalar_dtypes, nested_dtypes
@@ -166,8 +161,6 @@ def type_strategy_mapping(lookup: StratLookup=None) -> StratLookup:
             np.ndarray: arrays(scalar_dtypes(), array_shapes(max_dims=2)),
             np.dtype: nested_dtypes(),
         })
-    except ImportError:
-        pass
     return collections.ChainMap(dict(), known_type_strats, lookup or dict())
 
 
@@ -178,12 +171,12 @@ def strategy_from_generic_type(thing):
     The goal is for this to handle all such types in the standard library.
 
     TODO: implement the rest.  Work out how to test that it is in fact
-          exhaustive.
+          exhaustive.  See typing.__all__
 
     """
     assert isinstance(thing, typing.GenericMeta)
     # TODO: all of these assume that the dunder attribute is not None,
-    #       i.e. that we're seeign List[int] rather than plain List.
+    #       i.e. that we're seeing List[int] rather than plain List.
     #       This is not always the case and should be fixed.
     if issubclass(thing, Tuple):
         if hasattr(thing, '_field_types'):
@@ -207,27 +200,22 @@ def strategy_from_generic_type(thing):
     if issubclass(thing, Dict) or issubclass(thing, Mapping):
         keys, vals = (resolve(t) for t in thing.__args__)
         return st.dictionaries(keys, vals)
-    if issubclass(thing, Iterator)  or issubclass(thing, Iterable):
+    if issubclass(thing, (Iterator, Iterable)):
         # TODO: use iterables strategy once merged
         return st.lists(resolve(thing.__args__[0])).map(iter)
     if issubclass(thing, Generator):
         yieldtype, sendtype, returntype = thing.__args__
         def to_gen(alist):
-            yield from alist
+            for val in alist:
+                _ = yield val
             # TODO: proper drawing here so it minimises
             return resolve(returntype).example()
         return st.lists(resolve(yieldtype)).map(to_gen)
     if issubclass(thing, typing.Callable):
-        n_ary = len(thing.__args__)
-        def strat(value):
-            def func(*args):
-                assert len(args) == n_ary
-                return value
-            return func
         return resolve(thing.__result__).map(lambda v: (lambda *_: v))
 
     # TODO: support the rest of typing.__all__
-    raise NotImplementedError
+    raise ResolutionFailed('{} cannot be resolved to a type'.format(thing))
 
 
 def check_all_annotated(thing, lookup):
