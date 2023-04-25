3d78e180c6470775947510c0c64dbbaa38337ef5
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-04-25 14:28:00 -0600

    WIP

--- hypothesis-python/src/hypothesis/strategies/_internal/collections.py
+++ hypothesis-python/src/hypothesis/strategies/_internal/collections.py
@@ -11,6 +11,7 @@
 import copy
 from typing import Any, Iterable, Tuple, overload
 
+from hypothesis.control import current_build_context
 from hypothesis.errors import InvalidArgument
 from hypothesis.internal.conjecture import utils as cu
 from hypothesis.internal.conjecture.junkdrawer import LazySequenceCopy
@@ -28,6 +29,11 @@ from hypothesis.strategies._internal.strategies import (
     filter_not_satisfied,
 )
 from hypothesis.strategies._internal.utils import cacheable, defines_strategy
+from hypothesis.vendor.pretty import (
+    IDKey,
+    _dict_pprinter_factory,
+    _seq_pprinter_factory,
+)
 
 
 class TupleStrategy(SearchStrategy):
@@ -55,7 +61,16 @@ class TupleStrategy(SearchStrategy):
         return all(recur(e) for e in self.element_strategies)
 
     def do_draw(self, data):
-        return tuple(data.draw(e) for e in self.element_strategies)
+        context = current_build_context()
+        assert data is context.data
+        args, _, arg_slices = context.prep_args_kwargs_from_strategies(
+            self.element_strategies, {}
+        )
+        result = tuple(args)
+        context.known_object_printers[IDKey(result)].append(
+            _seq_pprinter_factory("(", ")", tuple, arg_slices)
+        )
+        return result
 
     def calc_is_empty(self, recur):
         return any(recur(e) for e in self.element_strategies)
@@ -299,7 +314,15 @@ class FixedKeysDictStrategy(MappedSearchStrategy):
         return f"FixedKeysDictStrategy({self.keys!r}, {self.mapped_strategy!r})"
 
     def pack(self, value):
-        return self.dict_type(zip(self.keys, value))
+        result = self.dict_type(zip(self.keys, value))
+        known_printers = current_build_context().known_object_printers
+        try:
+            known_printers[IDKey(result)].append(
+                _dict_pprinter_factory(known_printers[IDKey(value)].pop().arg_slices)
+            )
+        except Exception:  # pragma: no cover
+            pass  # just paranoia about evil user-defined dictionary types
+        return result
 
 
 class FixedAndOptionalKeysDictStrategy(SearchStrategy):
---
+++ hypothesis-python/tests/cover/test_inquisitor.py
@@ -0,0 +1,58 @@
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Copyright the Hypothesis Authors.
+# Individual contributors are listed in AUTHORS.rst and the git log.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+from hypothesis import given, strategies as st
+
+from ..conjecture.test_inquisitor import fails_with_output
+
+
+@fails_with_output(
+    """
+Falsifying example: test_inquisitor_comments_basic_fail_if_either(
+    a=False,  # or any other generated value
+    b=True,
+    c=[],  # or any other generated value
+    d=True,
+    e=False,  # or any other generated value
+)
+# The test always failed when commented parts were varied together.
+"""
+)
+@given(st.booleans(), st.booleans(), st.lists(st.none()), st.booleans(), st.booleans())
+def test_inquisitor_comments_basic_fail_if_either(a, b, c, d, e):
+    assert not (b and d)
+
+
+@fails_with_output(
+    """
+Falsifying example: test_inquisitor_comments_basic_fail_if_not_all(
+    a='',  # or any other generated value
+    b='',  # or any other generated value
+    c='',  # or any other generated value
+)
+# The test sometimes passed when commented parts were varied together.
+"""
+)
+@given(st.text(), st.text(), st.text())
+def test_inquisitor_comments_basic_fail_if_not_all(a, b, c):
+    assert a and b and c
+
+
+@fails_with_output(
+    """
+Falsifying example: test_inquisitor_no_together_comment_if_single_argument(
+    a='',
+    b='',  # or any other generated value
+)
+"""
+)
+@given(st.text(), st.text())
+def test_inquisitor_no_together_comment_if_single_argument(a, b):
+    assert a
